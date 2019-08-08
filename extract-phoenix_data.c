0de735f53a3a52534221f8de28a450e7ea9a182c
Missingmew <nintendo6496@googlemail.com>
2019-08-08 21:15:41 +0200

    start major refactor of extract-phoenix_data, adjust mapfiles accordingly, build most of the maps for phoenix 2 and 3

--- extract-phoenix_data.c
+++ extract-phoenix_data.c
@@ -31,8 +31,21 @@ typedef struct {
 	uint32_t paletteSize;
 }__attribute__((packed)) controlHeader;
 
+typedef struct {
+	unsigned int datasize;
+	unsigned int fileoffset;
+	unsigned int filesize;
+	char *palpath;
+	char *gfxpath;
+	unsigned int width;
+	unsigned int height;
+	unsigned int bpp;
+	unsigned int sourcetype;
+	unsigned int sourceoffset;
+} outputinfo;
+
 char *typestring[] = {
-	"rawdump", "rawgraphics", "compressedgraphics", "stripedgraphics", "patchgraphics", "uncompressedheadergraphics"
+	"rawdump", "compressedgraphics", "stripedgraphics", "patchgraphics", "uncompressedheadergraphics"
 };
 
 void makepath(char *path) {
@@ -52,7 +65,7 @@ void makepath(char *path) {
 	
 	while(token) {
 		createDirectory(curpath);
-		printf("creating directory %s\n", curpath);
+		//~ printf("creating directory %s\n", curpath);
 		strcat(curpath, "/");
 		token = strtok(NULL, "/");
 		strcat(curpath, token);
@@ -64,97 +77,108 @@ void makepath(char *path) {
 	return;
 }
 
-void extractTiledRawGraphic(FILE *f, unsigned int offset, char *path, unsigned int width, unsigned int height, unsigned int bpp, unsigned int paletteoffset) {
-	unsigned int palettesize;
-	unsigned int pixelsize;
-	unsigned int filesize;
-	unsigned char *workbuf = NULL, *palettebuf = NULL, *rgbapixeldata = NULL;
-	pixelsize = width*8 * height*8;
-	if(bpp == image4bpp) {
-		palettesize = 16*2;
-		pixelsize /= 2;
-	}
-	else if(bpp == image8bpp) {
-		palettesize = 256*2;
-	}
-	filesize = pixelsize * palettesize;
-	if(!paletteoffset) {
-		workbuf = malloc(filesize);
-		fseek(f, offset, SEEK_SET);
-		fread(workbuf, filesize, 1, f);
-		rgbapixeldata = tiledImageToRGBA(workbuf, width, height, bpp, 1);
-	}
-	else {
-		workbuf = malloc(pixelsize);
-		palettebuf = malloc(palettesize);
-		fseek(f, offset, SEEK_SET);
-		fread(workbuf, pixelsize, 1, f);
-		fseek(f, paletteoffset, SEEK_SET);
-		fread(palettebuf, palettesize, 1, f);
-		rgbapixeldata = tiledImageWithPaletteToRGBA(workbuf, palettebuf, width, height, bpp, 1);
-		free(palettebuf);
-	}
+void dumpRaw(FILE *f, outputinfo *info) {
+	FILE *o;
+	unsigned char *tempbuf = NULL;
 	
-	lodepng_encode32_file(path, rgbapixeldata, width*8, height*8);
-	free(workbuf);
-	free(rgbapixeldata);
-	return;
+	makepath(info->gfxpath);
+	
+	if(!(o = fopen(info->gfxpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->gfxpath);
+		return;
+	}
+	fseek(f, info->fileoffset, SEEK_SET);
+	tempbuf = malloc(info->filesize);
+	fread(tempbuf, info->filesize, 1, f);
+	fwrite(tempbuf, info->filesize, 1, o);
+	free(tempbuf);
+	fclose(o);
 }
 
-void extractCompressedGraphic(FILE *f, unsigned int datasize, unsigned int offset, char *path, unsigned int width, unsigned int height, unsigned int bpp) {
+void dumpCompressed(FILE *f, outputinfo *info) {
+	FILE *o;
 	uint32_t tempsize = 0;
-	unsigned char *workbuf = NULL, *resultbuf = NULL, *rgbapixeldata = NULL;
+	unsigned char *workbuf = NULL, *resultbuf = NULL;
 	unsigned int ressize, comsize;
-	fseek(f, offset+1, SEEK_SET);
+	
+	makepath(info->palpath);
+	makepath(info->gfxpath);
+	
+	// there is probably a better way to calculate the compressed size, even if we assume >100% compression ratio
+	fseek(f, info->fileoffset+1, SEEK_SET);
 	fread(&tempsize, 3, 1, f);
-	if((offset + 2*tempsize) > datasize) tempsize = datasize - offset;
+	if((info->fileoffset + 2*tempsize) > info->datasize) tempsize = info->datasize - info->fileoffset;
 	else tempsize *= 2;
-	fseek(f, offset, SEEK_SET);
+	comsize = tempsize;
 	workbuf = malloc(tempsize);
+	
+	fseek(f, info->fileoffset, SEEK_SET);
 	fread(workbuf, tempsize, 1, f);
 	resultbuf = unpackBuffer(workbuf, &ressize, &comsize);
-	if(bpp == image8bpp) {
-		if((width*8*height*8+512) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
+	if(info->bpp == image8bpp) {
+		if((info->width*8*info->height*8+512) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
 	}
-	else if(bpp == image4bpp) {
-		if(((width*8*height*8)/2+32) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
+	else if(info->bpp == image4bpp) {
+		if(((info->width*8*info->height*8)/2+32) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
 	}
 	if(!resultbuf) VOIDBREAK("couldnt decompress\n");
-	rgbapixeldata = tiledImageToRGBA(resultbuf, width, height, bpp, 1);
-	lodepng_encode32_file(path, rgbapixeldata, width*8, height*8);
+	
+	if(!(o = fopen(info->palpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->palpath);
+		return;
+	}
+	fwrite(resultbuf, (info->bpp == image8bpp) ? 512 : 32, 1, o);
+	fclose(o);
+	
+	if(!(o = fopen(info->gfxpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->gfxpath);
+		return;
+	}
+	if(info->bpp == image8bpp) fwrite(resultbuf+512, (info->width*8*info->height*8), 1, o);
+	else  fwrite(resultbuf+32, (info->width*8*info->height*8)/2, 1, o);
+	fclose(o);
+	
 	free(workbuf);
 	free(resultbuf);
-	free(rgbapixeldata);
 	return;
 }
 
-void extractStripedGraphic(FILE *f, unsigned int offset, char *path, unsigned int width, unsigned int height) {
+void dumpStriped(FILE *f, outputinfo *info) {
+	FILE *o;
 	pacEntry *entrylist;
 	uint32_t numfiles;
-	unsigned char *workbuf = NULL, *resultbuf = NULL, *rgbapixeldata = NULL, *palettedata = NULL, *fullbuffer = NULL, *fulltarget = NULL;
-	unsigned int ressize, comsize, bpp, i;
-	fseek(f, offset, SEEK_SET);
+	unsigned char *workbuf = NULL, *resultbuf = NULL, *palettedata = NULL, *fullbuffer = NULL, *fulltarget = NULL;
+	unsigned int ressize, comsize, bpp, i, fullsize;
+	
+	makepath(info->palpath);
+	makepath(info->gfxpath);
+	
+	fseek(f, info->fileoffset, SEEK_SET);
 	fread(&numfiles, 4, 1, f);
 	entrylist = malloc(sizeof(pacEntry)*numfiles);
 	fread(entrylist, sizeof(pacEntry), numfiles, f);
+	
 	palettedata = malloc(entrylist[0].size);
 	bpp = (entrylist[0].size == 32)?image4bpp:image8bpp;
-	fseek(f, offset+entrylist[0].offset, SEEK_SET);
+	fseek(f, info->fileoffset+entrylist[0].offset, SEEK_SET);
 	fread(palettedata, entrylist[0].size, 1, f);
 	
-	fullbuffer = malloc(width*8*((numfiles-1)*height)*8);
+	fullsize = info->width*8*((numfiles-1)*info->height)*8;
+	if(bpp == image4bpp) fullsize /= 2;
+	fullbuffer = malloc(fullsize);
 	fulltarget = fullbuffer;
 	
 	for(i=1;i < numfiles;i++) {
-		fseek(f, offset+entrylist[i].offset, SEEK_SET);
+		fseek(f, info->fileoffset+entrylist[i].offset, SEEK_SET);
 		workbuf = malloc(entrylist[i].size);
 		fread(workbuf, entrylist[i].size, 1, f);
+		comsize = entrylist[i].size;
 		resultbuf = unpackBuffer(workbuf, &ressize, &comsize);
 		if(bpp == image8bpp) {
-			if((width*8*height*8) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
+			if((info->width*8*info->height*8) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
 		}
 		else if(bpp == image4bpp) {
-			if(((width*8*height*8)/2) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
+			if(((info->width*8*info->height*8)/2) != ressize) VOIDBREAK("size/bpp dont match with uncompressed size\n");
 		}
 		if(!resultbuf) VOIDBREAK("couldnt decompress\n");
 		memcpy(fulltarget, resultbuf, ressize);
@@ -162,77 +186,83 @@ void extractStripedGraphic(FILE *f, unsigned int offset, char *path, unsigned in
 		free(workbuf);
 		free(resultbuf);
 	}
-	rgbapixeldata = tiledImageWithPaletteToRGBA(fullbuffer, palettedata, width, ((numfiles-1)*height), bpp, 1);
-	lodepng_encode32_file(path, rgbapixeldata, width*8, ((numfiles-1)*height)*8);
+	
+	if(!(o = fopen(info->palpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->palpath);
+		return;
+	}
+	fwrite(palettedata, entrylist[0].size, 1, o);
+	fclose(o);
+	
+	strcat(info->gfxpath, (bpp == image8bpp) ? ".8bpp" : ".4bpp");
+	if(!(o = fopen(info->gfxpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->gfxpath);
+		return;
+	}
+	fwrite(fullbuffer, fullsize, 1, o);
+	fclose(o);
+	
 	free(fullbuffer);
-	free(rgbapixeldata);
 	free(palettedata);
 	free(entrylist);
 	return;
 }
 
-void extractPatchGraphic(FILE *f, unsigned int datasize, unsigned int offset, char *path, unsigned int width, unsigned int height, unsigned int sourceoffset, unsigned int sourcetype) {
+void dumpPatch(FILE *f, outputinfo *info) {
 	pacEntry entry;
 	uint32_t tempsize = 0;
-	unsigned char *workbuf = NULL, *resultbuf = NULL, *rgbapixeldata = NULL, *palettedata = NULL;
+	unsigned char *workbuf = NULL, *resultbuf = NULL;
 	unsigned int ressize, comsize, bpp;
-	if(!sourcetype) {
-		fseek(f, sourceoffset+4, SEEK_SET);
+	FILE *o;
+	
+	makepath(info->gfxpath);
+	
+	if(!info->sourcetype) {
+		fseek(f, info->sourceoffset+4, SEEK_SET);
 		fread(&entry, sizeof(entry), 1, f);
-		palettedata = malloc(entry.size);
 		bpp = (entry.size == 32)?image4bpp:image8bpp;
-		fseek(f, sourceoffset+entry.offset, SEEK_SET);
-		fread(palettedata, entry.size, 1, f);
 	}
 	else {
-		bpp = (sourcetype==8)?image8bpp:image4bpp;
-		fseek(f, sourceoffset+1, SEEK_SET);
-		fread(&tempsize, 3, 1, f);
-		if((sourceoffset + 2*tempsize) > datasize) tempsize = datasize - sourceoffset;
-		else tempsize *= 2;
-		fseek(f, sourceoffset, SEEK_SET);
-		workbuf = malloc(tempsize);
-		fread(workbuf, tempsize, 1, f);
-		resultbuf = unpackBuffer(workbuf, &ressize, &comsize);
-		if(!resultbuf) {
-			printf("couldnt decompress\n");
-			return;
-		}
-		tempsize = (bpp == image4bpp)?32:512;
-		palettedata = malloc(tempsize);
-		memcpy(palettedata, resultbuf, tempsize);
-		free(workbuf);
-		free(resultbuf);
+		bpp = (info->sourcetype==8)?image8bpp:image4bpp;
 	}
-	fseek(f, offset+1, SEEK_SET);
+	fseek(f, info->fileoffset+1, SEEK_SET);
 	fread(&tempsize, 3, 1, f);
-	if((offset + 2*tempsize) > datasize) tempsize = datasize - offset;
+	if((info->fileoffset + 2*tempsize) > info->datasize) tempsize = info->datasize - info->fileoffset;
 	else tempsize *= 2;
-	fseek(f, offset, SEEK_SET);
+	comsize = tempsize;
+	
+	fseek(f, info->fileoffset, SEEK_SET);
 	workbuf = malloc(tempsize);
 	fread(workbuf, tempsize, 1, f);
 	resultbuf = unpackBuffer(workbuf, &ressize, &comsize);
 	if(bpp == image8bpp) {
-		if((width*8*height*8) != ressize) VOIDBREAK("size/bpp8 dont match with uncompressed size\n");
+		if((info->width*8*info->height*8) != ressize) VOIDBREAK("size/bpp8 dont match with uncompressed size\n");
 	}
 	else if(bpp == image4bpp) {
-		if(((width*8*height*8)/2) != ressize) VOIDBREAK("size/bpp4 dont match with uncompressed size\n");
+		if(((info->width*8*info->height*8)/2) != ressize) VOIDBREAK("size/bpp4 dont match with uncompressed size\n");
 	}
 	if(!resultbuf) VOIDBREAK("couldnt decompress\n");
-	rgbapixeldata = tiledImageWithPaletteToRGBA(resultbuf, palettedata, width, height, bpp, 0);
-	lodepng_encode32_file(path, rgbapixeldata, width*8, height*8);
-	free(rgbapixeldata);
+	
+	if(!(o = fopen(info->gfxpath, "wb"))) {
+		printf("dumpRaw: Couldnt open %s for writing\n", info->gfxpath);
+		return;
+	}
+	fwrite(resultbuf, ressize, 1, o);
+	fclose(o);
+	
 	free(workbuf);
 	free(resultbuf);
-	free(palettedata);
 	return;
 }
 
-void extractUncompressedHeaderGraphic(FILE *f, unsigned int offset, char *path) {
+void dumpUncompressed(FILE *f, outputinfo *info) {
 	controlHeader header;
 	unsigned char *workbuf = NULL, *palettedata = NULL, *rgbapixeldata = NULL;
 	unsigned int pixelsX, pixelsY, bpp;
-	fseek(f, offset, SEEK_SET);
+	
+	makepath(info->gfxpath);
+	
+	fseek(f, info->fileoffset, SEEK_SET);
 	fread(&header, sizeof(header), 1, f);
 	
 	/* lots of copypasta, see convert-uncompressed-image-header.c */
@@ -254,21 +284,21 @@ void extractUncompressedHeaderGraphic(FILE *f, unsigned int offset, char *path) 
 	switch( header.magic[0] ) {
 		/* 4bpp */
 		case 3: {
-			printf("image is 4bpp\n");
+			//~ printf("image is 4bpp\n");
 			bpp = image4bpp;
 			if((header.imageSize) != ((pixelsX*pixelsY)/2)) VOIDBREAK("size doesnt match!\n");
 			break;
 		}
 		/* 8bpp */
 		case 4: {
-			printf("image is 8bpp\n");
+			//~ printf("image is 8bpp\n");
 			bpp = image8bpp;
 			if((header.imageSize) != ((pixelsX*pixelsY))) VOIDBREAK("size doesnt match!\n");
 			break;
 		}
 		/* a5i3 */
 		case 6: {
-			printf("image is a5i3\n");
+			//~ printf("image is a5i3\n");
 			bpp = imagea5i3;
 			if((header.imageSize) != ((pixelsX*pixelsY))) VOIDBREAK("size doesnt match!\n");
 			break;
@@ -301,38 +331,45 @@ void extractUncompressedHeaderGraphic(FILE *f, unsigned int offset, char *path) 
 		memset(palettedata, 0, 32*2);
 	}
 	else palettedata = malloc(header.paletteSize);
-	fseek(f, offset+header.paletteOffset, SEEK_SET);
+	fseek(f, info->fileoffset+header.paletteOffset, SEEK_SET);
 	fread(palettedata, header.paletteSize, 1, f);
-	fseek(f, offset+header.headerSize, SEEK_SET);
+	fseek(f, info->fileoffset+header.headerSize, SEEK_SET);
 	workbuf = malloc(header.imageSize);
 	fread(workbuf, header.imageSize, 1, f);
 	
 	rgbapixeldata = linearImageWithPaletteToRGBA(workbuf, palettedata, pixelsX, pixelsY, bpp, 1);
-	lodepng_encode32_file(path, rgbapixeldata, pixelsX, pixelsY);
+	lodepng_encode32_file(info->gfxpath, rgbapixeldata, pixelsX, pixelsY);
 	free(workbuf);
 	free(rgbapixeldata);
 	free(palettedata);
 	return;
 }
-	
+
+void (*dumpfuncs[])(FILE *, outputinfo *) = {
+	dumpRaw,
+	dumpCompressed,
+	dumpStriped,
+	dumpPatch,
+	dumpUncompressed
+};
 
 int main(int argc, char** argv) {
-	
-	if ( argc < 3 ) {
-		printf("Not enough arguments given!\nUsage: %s file textmap\n", argv[0]);
-		return 1;
-	}
-	
-	FILE *data = NULL, *map = NULL, *o = NULL;
+	FILE *data = NULL, *map = NULL;
 	char mapline[2048] = {0};
-	unsigned char *tempbuf = NULL;
 	/* arguments used by all types */
-	char outputpath[2048] = {0};
+	char outputpath[2048] = {0}, palettepath[2048] = {0}, graphicspath[2048] = {0};
 	unsigned int i = 0, params = 0, datasize = 0, fileoffset = 0, filetype = 0;
 	
 	/* arguments used by specific types */
 	unsigned int filesize;
 	unsigned int tilesx, tilesy, sourcetype, sourceoffset, bpp;
+	
+	outputinfo outinfo;
+	
+	if ( argc < 3 ) {
+		printf("Not enough arguments given!\nUsage: %s file textmap\n", argv[0]);
+		return 1;
+	}
 	
 	if( !(data = fopen(argv[1], "rb"))) {
 		printf("Couldnt open %s for reading\n", argv[1]);
@@ -364,73 +401,86 @@ int main(int argc, char** argv) {
 			printf("Malformed params at line %d, wanted 3, got %d\n", i, params);
 			continue;
 		}
-		if(strlen(outputpath)<3) {
-			printf("invalid path at line %d (%u)\n", i, (unsigned int)strlen(outputpath));
-			continue;
-		}
 		printf("line %03d - Offset %08x - Type %s(%d) - %s\n", i, fileoffset, typestring[filetype], filetype, outputpath);
-		makepath(outputpath);
 		switch(filetype) {
 			case 0: { /* rawdump */
-				params = sscanf(mapline, "%x %d \"%[^\"]\" %x", &fileoffset, &filetype, outputpath, &filesize);
+				params = sscanf(mapline, "%x %d \"%[^\"]\" %x", &fileoffset, &filetype, graphicspath, &filesize);
 				if(params < 4) {
 					printf("Not enough params for rawdump at line %d, wanted 4, got %d\n", i, params);
 					continue;
 				}
-				if(!(o = fopen(outputpath, "wb"))) {
-					printf("Couldnt open %s for writing\n", outputpath);
-					break;
+				if(strlen(graphicspath)<3) {
+					printf("invalid output path at line %d (%u)\n", i, (unsigned int)strlen(graphicspath));
+					continue;
 				}
-				fseek(data, fileoffset, SEEK_SET);
-				tempbuf = malloc(filesize);
-				fread(tempbuf, filesize, 1, data);
-				fwrite(tempbuf, filesize, 1, o);
-				free(tempbuf);
-				fclose(o);
 				break;
 			}
-			case 1: { /* tiled rawgraphics */
-				printf("Raw graphics arent supported right now\n");
-				/* if paletteoffset is 0, assume palette in front of image data */
-				/* params = sscanf(mapline, "%x %d \"%[^\"]\" %d %d %x", &fileoffset, &filetype, outputpath, &tilesx, &tilesy, &sourcetype, &paletteoffset);
-				bpp = (sourcetype == 8)?image8bpp:image4bpp;
-				extractTiledRawRaphic(data, fileoffset, outputpath, tilesx, tilesy, bpp, paletteoffset);*/
-				break;
-			}
-			case 2: { /* compressed graphics */
-				params = sscanf(mapline, "%x %d \"%[^\"]\" %d %d %d", &fileoffset, &filetype, outputpath, &tilesx, &tilesy, &sourcetype);
-				if(params < 6) {
-					printf("Not enough params for comgfx at line %d, wanted 6, got %d\n", i, params);
+			case 1: { /* compressed graphics */
+				params = sscanf(mapline, "%x %d \"%[^\"]\" \"%[^\"]\" %d %d %d", &fileoffset, &filetype, palettepath, graphicspath, &tilesx, &tilesy, &sourcetype);
+				if(params < 7) {
+					printf("Not enough params for comgfx at line %d, wanted 7, got %d\n", i, params);
+					continue;
+				}
+				if(strlen(palettepath)<3) {
+					printf("invalid palette path at line %d (%u)\n", i, (unsigned int)strlen(palettepath));
+					continue;
+				}
+				if(strlen(graphicspath)<3) {
+					printf("invalid graphics path at line %d (%u)\n", i, (unsigned int)strlen(graphicspath));
 					continue;
 				}
 				bpp = (sourcetype == 8)?image8bpp:image4bpp;
-				extractCompressedGraphic(data, datasize, fileoffset, outputpath, tilesx, tilesy, bpp);
 				break;
 			}
-			case 3: { /* striped graphics */
-				params = sscanf(mapline, "%x %d \"%[^\"]\" %d %d", &fileoffset, &filetype, outputpath, &tilesx, &tilesy);
-				if(params < 5) {
+			case 2: { /* striped graphics */
+				params = sscanf(mapline, "%x %d \"%[^\"]\" \"%[^\"]\" %d %d", &fileoffset, &filetype, palettepath, graphicspath, &tilesx, &tilesy);
+				if(params < 6) {
 					printf("Not enough params for sgfx at line %d, wanted 5, got %d\n", i, params);
 					continue;
 				}
-				extractStripedGraphic(data, fileoffset, outputpath, tilesx, tilesy);
+				if(strlen(palettepath)<3) {
+					printf("invalid palette path at line %d (%u)\n", i, (unsigned int)strlen(palettepath));
+					continue;
+				}
+				if(strlen(graphicspath)<3) {
+					printf("invalid graphics path at line %d (%u)\n", i, (unsigned int)strlen(graphicspath));
+					continue;
+				}
 				break;
 			}
-			case 4: { /* patch graphics */
-				params = sscanf(mapline, "%x %d \"%[^\"]\" %d %d %x %d", &fileoffset, &filetype, outputpath, &tilesx, &tilesy, &sourceoffset, &sourcetype);
+			case 3: { /* patch graphics */
+				params = sscanf(mapline, "%x %d \"%[^\"]\" %d %d %x %d", &fileoffset, &filetype, graphicspath, &tilesx, &tilesy, &sourceoffset, &sourcetype);
 				if(params < 7) {
 					printf("Not enough params for pgfx at line %d, wanted 7, got %d\n", i, params);
 					continue;
 				}
-				extractPatchGraphic(data, datasize, fileoffset, outputpath, tilesx, tilesy, sourceoffset, sourcetype);
+				if(strlen(graphicspath)<3) {
+					printf("invalid output path at line %d (%u)\n", i, (unsigned int)strlen(graphicspath));
+					continue;
+				}
 				break;
 			}
-			case 5: { /*uncompressed header graphics */
+			case 4: { /*uncompressed header graphics */
 				/* dont need to sscanf again, since we dont need additional data if everything is okay */
-				extractUncompressedHeaderGraphic(data, fileoffset, outputpath);
 				break;
+			}
+			default: {
+				printf("unknown type %d in line %d, skipping...\n", filetype, i);
+				continue;
 			}
 		}
+		outinfo.datasize = datasize;
+		outinfo.fileoffset = fileoffset;
+		outinfo.filesize = filesize;
+		outinfo.palpath = palettepath;
+		outinfo.gfxpath = graphicspath;
+		outinfo.width = tilesx;
+		outinfo.height = tilesy;
+		outinfo.bpp = bpp;
+		outinfo.sourcetype = sourcetype;
+		outinfo.sourceoffset = sourceoffset;
+		
+		dumpfuncs[filetype](data, &outinfo);
 	}
 	fclose(data);
 	fclose(map);
