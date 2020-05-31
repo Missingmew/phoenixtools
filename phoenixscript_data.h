54ae4d362468e1f4b90c6233fc5a40da2674a782
Missingmew <nintendo6496@googlemail.com>
2020-05-31 12:29:14 +0200

    add missing definition of musicpause to header

--- phoenixscript_data.h
+++ phoenixscript_data.h
@@ -6,6 +6,7 @@ extern char *colors[4];
 extern char *shiftdirection[4];
 extern char *backgrounds[4][4096];
 extern char *musicfading[2];
+extern char *musicpause[2];
 extern char *soundplay[2];
 extern char *showside[2];
 extern char *testimonypress[2];
