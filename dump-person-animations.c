faf5095b0328fd7122d287412a7329b6629b94a4
Missingmew <nintendo6496@googlemail.com>
2020-06-16 05:24:49 +0200

    add filelist support to extract-archive, fix output of dump-person-animations, add sanity checks, add proper support for dual palette animations in case 5 (some fields not entirely clear yet, needs more investigation)

--- dump-person-animations.c
+++ dump-person-animations.c
@@ -4,6 +4,18 @@
 #include "lodepng/lodepng.h"
 #include "phoenixgfx.h"
 
+#define NDS_WIDTH 256
+#define NDS_HEIGHT 192
+#define NDS_IMAGESIZE (NDS_WIDTH*NDS_HEIGHT)
+#define NDS_CENTER_X (NDS_WIDTH/2)
+#define NDS_CENTER_Y (NDS_HEIGHT/2)
+
+#define GBA_WIDTH 240
+#define GBA_HEIGHT 160
+#define GBA_IMAGESIZE (GBA_WIDTH*GBA_HEIGHT)
+#define GBA_CENTER_X (GBA_WIDTH/2)
+#define GBA_CENTER_Y (GBA_HEIGHT/2)
+
 struct objsize {
 	unsigned w, h;
 };
@@ -36,15 +48,18 @@ struct objsize sizelut[3][4] = {
 
 struct animetatile {
 	signed x, y;
-	unsigned w, h;
+	signed w, h; /* while never < 0, will make sanitychecks castless */
 	unsigned index;
+	unsigned palette;
 };
 
 struct partdesc {
 	signed x:8;
 	signed y:8;
-	unsigned tilenum:10;
-	unsigned pad:2;
+	unsigned tilenum:8; // was 10
+	unsigned pad:3;
+	//~ unsigned pad:2;
+	unsigned whichpal:1; /* ? */
 	unsigned shape:2;
 	unsigned size:2;
 }__attribute__((packed));
@@ -62,6 +77,11 @@ struct animationframe {
 	uint32_t null;
 }__attribute__((packed));
 
+void copynonzero(unsigned char *dest, unsigned char *src, unsigned count) {
+	for(unsigned i = 0; i < count; i++, src++, dest++) {
+		if(*src) *dest = *src;
+	}
+}
 
 unsigned char *deRLE(uint16_t *src, unsigned decompressedsize) {
 	uint16_t *ret = malloc(decompressedsize);
@@ -87,7 +107,9 @@ unsigned char *deRLE(uint16_t *src, unsigned decompressedsize) {
 void blit_metatile(unsigned char *dest, uint16_t **src, struct animetatile *meta, unsigned imagewidth, unsigned bpp) {
 	unsigned tilesize;
 	unsigned h;
-	unsigned char *blitdest = NULL, *srctiles = NULL, *srclinear = NULL;
+	unsigned char *blitdest = NULL, *blitsrc = NULL, *srctiles = NULL, *srclinear = NULL;
+	unsigned xofs = 0, yofs = 0;
+	unsigned xlimit = 0, ylimit = 0;
 	
 	unsigned metawtiles, metahtiles;
 	metawtiles = meta->w/8;
@@ -100,32 +122,47 @@ void blit_metatile(unsigned char *dest, uint16_t **src, struct animetatile *meta
 		return;
 	}
 	
+	//~ printf("meta: x=%d y=%d w=%u(%u) h=%u(%u) idx %u\n", meta->x, meta->y, meta->w, metawtiles, meta->h, metahtiles, meta->index);
+	//~ printf("%u %u %u %u\n", meta->x+meta->w < 0, meta->x >= NDS_WIDTH, meta->y+meta->h < 0, meta->y >= NDS_HEIGHT);
+	/* check if there is anything to be drawn within bounds */
+	if(meta->x+meta->w < 0) return;
+	if(meta->x >= NDS_WIDTH) return;
+	if(meta->y+meta->h < 0) return;
+	if(meta->y >= NDS_HEIGHT) return;
+	
+	/* adjust x and y offset to be within screen bounds, skip everything outside */
+	if(meta->x < 0) xofs = meta->x * -1;
+	if(meta->x+meta->w >= NDS_WIDTH) xlimit = meta->x+meta->w - NDS_WIDTH;
+	
+	if(meta->y < 0) yofs = meta->y * -1;
+	if(meta->y+meta->h >= NDS_HEIGHT) ylimit = meta->y+meta->h - NDS_HEIGHT;
+	
+	/* decompress and convert to linear to allow easy off-grid printing */
 	srctiles = deRLE(src[meta->index], metawtiles*metahtiles*tilesize);
 	srclinear = generateIndexedImageFromTiles(srctiles, metawtiles, metahtiles, bpp, NULL);
 	
-	blitdest = dest + meta->x + imagewidth*meta->y;
-	//~ printf("dest %p src %p mets %p imw %u bpp %u\n", dest, src, meta, imagewidth, bpp);
-	//~ printf("skipsize %u tilesize %u blitdest %p (offset by %u)\n", skipsize, tilesize, blitdest, metaxtiles*tilesize + skipsize*metaytiles);
-	//~ printf("meta: x=%u(%u) y=%u(%u) w=%u(%u) h=%u(%u) idx %u\n", meta->x, metaxtiles, meta->y, metaytiles, meta->w, metawtiles, meta->h, metahtiles, meta->index);
-	for(h = 0; h < meta->h; h++) {
-		memcpy(blitdest+h*imagewidth, srclinear+h*meta->w, meta->w);
+	if(meta->palette && bpp == image4bpp) {
+		for(unsigned i = 0; i < meta->w*meta->h; i++) if(srclinear[i]) srclinear[i] += 0x10;
+	}
+	
+	blitdest = dest + meta->x+xofs + imagewidth*(meta->y+yofs);
+	blitsrc = srclinear + xofs + meta->w*yofs;
+	
+	//~ printf("dest %p src %p meta %p imw %u bpp %u\n", dest, src, meta, imagewidth, bpp);
+	//~ printf("tilesize %u blitdest %p (offset by %u)\n", tilesize, blitdest, meta->x + imagewidth*meta->y);
+	//~ printf("xofs=%u xlimit=%u yofs=%u ylimit=%u\n", xofs, xlimit, yofs, ylimit);
+	
+	for(h = 0; h < meta->h-yofs-ylimit; h++) {
+		copynonzero(blitdest+h*imagewidth, blitsrc+h*meta->w, meta->w-xofs-xlimit);
 	}
 	
 	free(srctiles);
 	free(srclinear);
 }
 
-#define NDS_WIDTH 256
-#define NDS_HEIGHT 192
-#define NDS_IMAGESIZE (NDS_WIDTH*NDS_HEIGHT)
-#define NDS_CENTER_X (NDS_WIDTH/2)
-#define NDS_CENTER_Y (NDS_HEIGHT/2)
-
-#define GBA_WIDTH 240
-#define GBA_HEIGHT 160
-#define GBA_IMAGESIZE (GBA_WIDTH*GBA_HEIGHT)
-#define GBA_CENTER_X (GBA_WIDTH/2)
-#define GBA_CENTER_Y (GBA_HEIGHT/2)
+unsigned isvalidtable(struct animationhead *head) {
+	return !(head->null + head->pad);
+}
 
 int main(int argc, char **argv) {
 	FILE *gfx = NULL, *anim = NULL;
@@ -135,11 +172,13 @@ int main(int argc, char **argv) {
 	void *gfxbuf = NULL, *animbuf = NULL;
 	
 	unsigned numpals;
-	unsigned char **paldata = NULL;
+	unsigned char *palbase = NULL, *paldata = NULL;
+	//~ unsigned char **paldata = NULL;
 	uint16_t **tiledata = NULL;
 	void *gfxbase = NULL;
 	uint32_t *partslist = NULL;
 	
+	unsigned numtotalframes;
 	struct animationhead *animhead = NULL;
 	struct animationframe *animframes = NULL;
 	uint32_t *numparts;
@@ -165,8 +204,12 @@ int main(int argc, char **argv) {
 	fclose(gfx);
 	/* NDS versions have MSB set for some reason, GBA ones dont */
 	numpals = *((uint32_t *)gfxbuf) & 0x7FFFFFFF;
-	paldata = malloc(sizeof(unsigned char *) * numpals);
-	for(unsigned i = 0; i < numpals; i++) paldata[i] = gfxbuf+4+i*0x20;
+	//~ paldata = malloc(sizeof(unsigned char *) * numpals);
+	//~ for(unsigned i = 0; i < numpals; i++) paldata[i] = gfxbuf+4+i*0x20;
+	paldata = malloc(0x100*2);
+	memset(paldata, 0, 0x100*2);
+	palbase = gfxbuf+4;
+	memcpy(paldata, palbase, numpals * 0x10*2);
 	partslist = gfxbase = gfxbuf+4+numpals*0x20;
 	tiledata = malloc((partslist[0] / 4)*sizeof(uint16_t *));
 	for(unsigned i = 0; i < (partslist[0] / 4); i++) tiledata[i] = gfxbase+partslist[i];
@@ -186,28 +229,49 @@ int main(int argc, char **argv) {
 	
 	printf("table in gfx file has %u entries\n", partslist[0] / 4);
 	
+	numtotalframes = 0;
 	/* only try first table for now */
 	unsigned currenttable, endoftable, lastoffset, lastparts;
-	for(currenttable = 0, endoftable = 0; endoftable < animsize; endoftable += lastoffset + lastparts * sizeof(uint32_t) + sizeof(uint32_t), currenttable++) {
+	for(currenttable = 0, endoftable = 0; endoftable+sizeof(struct animationhead)-1 < animsize; endoftable += lastoffset + lastparts * sizeof(uint32_t) + sizeof(uint32_t), currenttable++) {
 		lastoffset = 0;
 		lastparts = 0;
-		printf("Table %u start at %08x\n", currenttable, endoftable);
 		animhead = animbuf+endoftable;
+		if(!isvalidtable(animhead)) {
+			printf("Table %u has extra frames after the last referenced one! First unreferenced data at %08x\n", currenttable-1, endoftable);
+			while(endoftable+sizeof(struct animationhead)-1 < animsize && !isvalidtable(animhead)) {
+				endoftable += 4;
+				animhead = animbuf+endoftable;
+			}
+			/* only when last table */
+			if(endoftable+sizeof(struct animationhead) >= animsize) break;
+		}
 		animframes = (void *)animhead+sizeof(struct animationhead);
+		printf("Table %u at %08x has %u animations\n", currenttable, endoftable, animhead->numentries);
+		numtotalframes += animhead->numentries;
 		
 		for(unsigned curframe = 0; curframe < animhead->numentries; curframe++) {
+			if(endoftable + animframes[curframe].offset >= animsize) {
+				printf("Table %u entry %u points to EOF!\n", currenttable, curframe);
+				endoftable += animframes[curframe].offset; // make sure to break out of the outer loop
+				break;
+			}
 			memset(finalimage, 0, NDS_IMAGESIZE);
 			printf("Frame %u dataoff %08x:\n", curframe, animframes[curframe].offset);
 			numparts = (void *)animhead + animframes[curframe].offset;
 			partheads = (void *)numparts + sizeof(uint32_t);
 			for(unsigned curpart = 0; curpart < *numparts; curpart++) {
 				struct animetatile metatile;
-				//~ printf("part %2x x=%4d y=%4d shape=%u size=%u pad=%u w=%2u h=%2u tilenum=%4u\n", curpart, partheads[curpart].x, partheads[curpart].y, partheads[curpart].shape, partheads[curpart].size, partheads[curpart].pad, sizelut[partheads[curpart].shape][partheads[curpart].size].w, sizelut[partheads[curpart].shape][partheads[curpart].size].h, partheads[curpart].tilenum);
+				//~ printf("part %2x x=%4d y=%4d shape=%u size=%u whichpal=%u pad=%u w=%2u h=%2u tilenum=%4u\n", curpart, partheads[curpart].x, partheads[curpart].y, partheads[curpart].shape, partheads[curpart].size, partheads[curpart].whichpal, partheads[curpart].pad, sizelut[partheads[curpart].shape][partheads[curpart].size].w, sizelut[partheads[curpart].shape][partheads[curpart].size].h, partheads[curpart].tilenum);
+				if(partheads[curpart].pad) printf("Table %u frame %u part %u has nonzero pad %x!\n", currenttable, curframe, curpart, partheads[curpart].pad);
+				if(partheads[curpart].tilenum >= partslist[0] / 4) printf("Table %u frame %u part %u has OOB index %u!\n", currenttable, curframe, curpart, partheads[curpart].tilenum);
+				//~ fflush(stdout);
 				metatile.x = partheads[curpart].x;
 				metatile.y = partheads[curpart].y;
 				metatile.w = sizelut[partheads[curpart].shape][partheads[curpart].size].w;
 				metatile.h = sizelut[partheads[curpart].shape][partheads[curpart].size].h;
 				metatile.index = partheads[curpart].tilenum;
+				metatile.palette = partheads[curpart].whichpal;
+				//~ metatile.palette = 0;
 				/* convert relative positions to absolute ones */
 				metatile.x += NDS_CENTER_X;
 				metatile.y += NDS_CENTER_Y;
@@ -215,7 +279,7 @@ int main(int argc, char **argv) {
 			}
 			
 			sprintf(finaloutputname, "%s-tb%02u-fr%02u.png", argv[3], currenttable, curframe);
-			finalrgba = linearImageWithPaletteToRGBA(finalimage, paldata[0], NDS_WIDTH, NDS_HEIGHT, image8bpp, 0);
+			finalrgba = linearImageWithPaletteToRGBA(finalimage, paldata, NDS_WIDTH, NDS_HEIGHT, image8bpp, 0);
 			lodepng_encode32_file(finaloutputname, finalrgba, NDS_WIDTH, NDS_HEIGHT);
 			free(finalrgba);
 			
@@ -226,6 +290,7 @@ int main(int argc, char **argv) {
 		}
 		//~ printf("lastoffset %08x lastparts %08x\n", lastoffset, lastparts);
 	}
+	printf("Found a total of %u tables with %u animations\n", currenttable, numtotalframes);
 	
 	free(finalimage);
 	free(tiledata);
--- extract-archive.c
+++ extract-archive.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "ntrcom/nitrocompression.h"
 
 typedef struct {
@@ -8,60 +9,87 @@ typedef struct {
 	uint32_t size;
 }__attribute__((packed)) pacEntry;
 
+typedef enum {
+	LINEUSE,
+	LINEEMPTY
+} linetype;
+
 int main( int argc, char** argv ) {
 	
 	if ( argc < 3 )
 	{
-		printf( "Not enough arguments given!\nUsage: %s [file] [ignore empty files]\nFiles will be extracted in current working directory.\n", argv[0] );
+		printf( "Not enough arguments given!\nUsage: %s [file] [enable decompressor] [list]\nFiles will be extracted in current working directory.\n", argv[0] );
 		return 1;
 	}
 	
-	uint32_t i, j, numFiles, listOffset, ignoreEmpty;
-	unsigned int resultsize, compressedsize, checksum;
-	ignoreEmpty = strtoul(argv[2], NULL, 10);
-	char outputname[32] = { 0 };
+	uint32_t i, numFiles;
+	unsigned int resultsize, compressedsize, decompress;
+	char outputname[256], mapline[256];
 	unsigned char *workbuffer = NULL, *resultbuffer = NULL;
-	pacEntry workentry;
-	FILE *f, *o;
+	pacEntry *entrylist = NULL;
+	linetype curline;
+	FILE *f, *o, *t = NULL;
+	
+	decompress = strtoul(argv[2], NULL, 10);
 	
 	if( !(f = fopen( argv[1], "rb" ))) {
 		printf("Couldnt open file %s\n", argv[1]);
 		return 1;
 	}
+	if(argc == 4) {
+		if( !(t = fopen( argv[3], "r" ))) {
+			printf("Couldnt open file %s\n", argv[3]);
+			return 1;
+		}
+	}
 	
 	fread( &numFiles, 4, 1, f );
+	entrylist = malloc(sizeof(pacEntry) * numFiles);
+	fread(entrylist, numFiles * sizeof(pacEntry), 1, f);
 	
+	curline = LINEEMPTY;
+	resultsize = 0;
 	for( i = 0; i < numFiles; i++ ) {
-		checksum = 0;
-		fread( &workentry, sizeof(pacEntry), 1, f );
-		printf("Current file offset: %08x - Current file size %08x\n", workentry.offset, workentry.size);
-		listOffset = ftell(f);
-		fseek( f, workentry.offset, SEEK_SET );
-		snprintf( outputname, 32, "%08d-%08x.bin", i, i );
-		workbuffer = malloc(workentry.size);
-		fread( workbuffer, workentry.size, 1, f );
-		resultbuffer = unpackBuffer( workbuffer, &resultsize, &compressedsize );
-		if(!resultbuffer) {
-			printf("file %08x is uncompressed!\n", i);
-			resultsize = 0;
+		printf("Current file offset: %08x - Current file size %08x\n", entrylist[i].offset, entrylist[i].size);
+		if(t) {
+			fgets(mapline, 256, t);
+			while(!(strncmp(mapline, "//", 2)) && !feof(t)) fgets(mapline, 256, t);
+			if(feof(t)) curline = LINEEMPTY;
+			else if(strlen(mapline) < 3) curline = LINEEMPTY;
+			else curline = LINEUSE;
 		}
-		for( j = 0; j < resultsize; j++ ) checksum += resultbuffer[j];
-		if( checksum || !ignoreEmpty ) {
-			if( !(o = fopen( outputname, "wb" ))) {
-				printf("Couldnt open file %s\n", outputname);
-				return 1;
+		
+		while(strcspn(mapline, "\r\n") < strlen(mapline)) mapline[strcspn(mapline,"\r\n")] = 0;
+		fseek( f, entrylist[i].offset, SEEK_SET );
+		workbuffer = malloc(entrylist[i].size);
+		fread( workbuffer, entrylist[i].size, 1, f );
+		
+		if(decompress) {
+			resultbuffer = unpackBuffer( workbuffer, &resultsize, &compressedsize );
+			if(!resultbuffer) {
+				printf("file %08x is uncompressed!\n", i);
+				resultsize = 0;
 			}
-			if(!resultsize) fwrite( workbuffer, workentry.size, 1, o );
-			else fwrite( resultbuffer, resultsize, 1, o );
-			fclose(o);
 		}
-		else printf("file is completely made up of nullbytes, skipping...\n");
+		
+		if(curline == LINEUSE) sprintf(outputname, "%.255s", mapline);
+		else sprintf( outputname, "%04d-%04x.bin", i, i );
+		
+		if( !(o = fopen( outputname, "wb" ))) {
+			printf("Couldnt open file %s\n", outputname);
+			return 1;
+		}
+		if(!resultsize) fwrite( workbuffer, entrylist[i].size, 1, o );
+		else fwrite( resultbuffer, resultsize, 1, o );
+		fclose(o);
+		
 		free( workbuffer );
 		if(resultsize) free( resultbuffer );
-		fseek( f, listOffset, SEEK_SET );
 	}
-	printf("Done.\n");
+	free(entrylist);
+	if(t) fclose(t);
 	fclose(f);
+	printf("Done.\n");
 	return 0;
 	
 }
