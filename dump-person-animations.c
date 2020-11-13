7f9ab1360cf57545333b2ebeed2dd08dafc74624
Missingmew <nintendo6496@googlemail.com>
2020-11-13 01:01:37 +0100

    make dump-person-animations GBA and plain tiles capable, restructure and add dumping of extra frames

--- dump-person-animations.c
+++ dump-person-animations.c
@@ -4,20 +4,11 @@
 #include "lodepng/lodepng.h"
 #include "phoenixgfx.h"
 
-#define NDS_WIDTH 256
-#define NDS_HEIGHT 192
-#define NDS_IMAGESIZE (NDS_WIDTH*NDS_HEIGHT)
-#define NDS_CENTER_X (NDS_WIDTH/2)
-#define NDS_CENTER_Y (NDS_HEIGHT/2)
-
-#define GBA_WIDTH 240
-#define GBA_HEIGHT 160
-#define GBA_IMAGESIZE (GBA_WIDTH*GBA_HEIGHT)
-#define GBA_CENTER_X (GBA_WIDTH/2)
-#define GBA_CENTER_Y (GBA_HEIGHT/2)
-
 #define PALSIZE4 (2*16)
 #define PALSIZE8 (2*256)
+
+#define MODE_GBA 0
+#define MODE_NDS 1
 
 struct objsize {
 	unsigned w, h;
@@ -49,6 +40,43 @@ struct objsize sizelut[3][4] = {
 	/* not allowed */
 };
 
+struct screendims {
+	signed width, height;
+	signed size;
+	signed centerx, centery;
+};
+
+struct screendims systemdimensions[2] = {
+	/* GBA */
+	{
+		.width = 240, .height = 160,
+		.size = 240*160,
+		.centerx = 240/2, .centery = 160/2
+	},
+	/* NDS */
+	{
+		.width = 256, .height = 192,
+		.size = 256*192,
+		.centerx = 256/2, .centery = 192/2
+	}
+};
+
+struct tiledatastate {
+	/* persistent state, set by hand */
+	unsigned mode;
+	
+	/* dynamic state, changed by setuptiledatastate */
+	unsigned statesrc;
+	
+	unsigned numpals;
+	unsigned isrle;
+	unsigned char *palbase, *paldata;
+	unsigned char *rawtiledata;
+	uint16_t **tiledata;
+	void *gfxbase;
+	uint32_t *partslist;
+};
+
 struct animetatile {
 	signed x, y;
 	signed w, h; /* while never < 0, will make sanitychecks castless */
@@ -57,13 +85,21 @@ struct animetatile {
 	unsigned basepalette;
 };
 
-struct partdesc {
+struct partdesc_gba {
+	signed x:8;
+	signed y:8;
+	unsigned tilenum:11;
+	unsigned palnum:1;
+	unsigned shape:2;
+	unsigned size:2;
+}__attribute__((packed));
+
+struct partdesc_nds {
 	signed x:8;
 	signed y:8;
 	unsigned tilenum:8; // was 10
 	unsigned pad:1;
 	unsigned palnum:2;
-	//~ unsigned pad:2;
 	unsigned twinpal:1; /* ? */
 	unsigned shape:2;
 	unsigned size:2;
@@ -72,7 +108,7 @@ struct partdesc {
 struct animationhead {
 	uint16_t null;
 	uint16_t numentries;
-	uint32_t pad;
+	uint32_t tiledataoff;
 }__attribute__((packed));
 
 struct animationframe {
@@ -81,6 +117,76 @@ struct animationframe {
 	uint8_t palmod;
 	uint32_t null;
 }__attribute__((packed));
+
+void part2animeta(struct animetatile *dst, void *src, unsigned mode) {
+	unsigned pad = 0;
+	switch(mode) {
+		case MODE_GBA: {
+			struct partdesc_gba *partsrc = src;
+			dst->x = partsrc->x;
+			dst->y = partsrc->y;
+			dst->w = sizelut[partsrc->shape][partsrc->size].w;
+			dst->h = sizelut[partsrc->shape][partsrc->size].h;
+			dst->index = partsrc->tilenum;
+			dst->twinpalette = 0;
+			dst->basepalette = partsrc->palnum;
+			break;
+		}
+		case MODE_NDS: {
+			struct partdesc_nds *partsrc = src;
+			dst->x = partsrc->x;
+			dst->y = partsrc->y;
+			dst->w = sizelut[partsrc->shape][partsrc->size].w;
+			dst->h = sizelut[partsrc->shape][partsrc->size].h;
+			dst->index = partsrc->tilenum;
+			dst->twinpalette = partsrc->twinpal;
+			dst->basepalette = partsrc->palnum;
+			pad = partsrc->pad;
+			break;
+		}
+		default: {
+			printf("%s: bad mode %u\n", __func__, mode);
+			return;
+		}
+	}
+	//~ printf("loaded part: x=%4d(%3d) y=%4d(%3d) twinpal=%u palnum=%u w=%2u h=%2u tilenum=%4u\n", dst->x, dst->x+systemdimensions[mode].centerx, dst->y, dst->y+systemdimensions[mode].centery, dst->twinpalette, dst->basepalette, dst->w, dst->h, dst->index);
+	if(pad) printf("part has nonzero pad!\n");
+}
+
+unsigned setuptiledatastate(struct tiledatastate *dst, void *src, unsigned offset) {
+	src += offset;
+	/* msb denotes wether graphics data is plain or RLE with index table */
+	dst->numpals = *((uint32_t *)src) & 0x7FFFFFFF;
+	dst->isrle = *((uint32_t *)src) & 0x80000000;
+	if(dst->numpals > 16) {
+		printf("%s: was about to attempt to load > 16 palettes (%u)!\n", __func__, dst->numpals);
+		return 0;
+	}
+	if(!dst->paldata) dst->paldata = malloc(0x100*2);
+	memset(dst->paldata, 0, 0x100*2);
+	dst->palbase = src+4;
+	memcpy(dst->paldata, dst->palbase, dst->numpals * 0x10*2);
+	if(dst->isrle) {
+		dst->partslist = dst->gfxbase = dst->palbase+dst->numpals*0x20;
+		if(dst->tiledata) {
+			free(dst->tiledata);
+			dst->tiledata = 0;
+		}
+		dst->tiledata = malloc((dst->partslist[0] / 4)*sizeof(uint16_t *));
+		for(unsigned i = 0; i < (dst->partslist[0] / 4); i++) dst->tiledata[i] = dst->gfxbase+dst->partslist[i];
+		printf("%s: graphics file has %u palettes and %u metatiles\n", __func__, dst->numpals, dst->partslist[0] / 4);
+	}
+	else {
+		if(dst->tiledata) {
+			free(dst->tiledata);
+			dst->tiledata = 0;
+		}
+		dst->rawtiledata = dst->gfxbase = dst->palbase+dst->numpals*0x20;
+		printf("%s: graphics file has %u palettes\n", __func__, dst->numpals);
+	}
+	dst->statesrc = offset;
+	return 1;
+}
 
 void copynonzero(unsigned char *dest, unsigned char *src, unsigned count) {
 	for(unsigned i = 0; i < count; i++, src++, dest++) {
@@ -109,9 +215,8 @@ unsigned char *deRLE(uint16_t *src, unsigned decompressedsize) {
 }
 
 /* stores tiles from src at dest according to meta, assumes all dimensions are in pixels */
-void blit_metatile(unsigned char *dest, uint16_t **src, struct animetatile *meta, unsigned imagewidth, unsigned bpp) {
+void blit_metatile(unsigned char *dest, struct animetatile *meta, unsigned bpp, struct tiledatastate *state) {
 	unsigned tilesize;
-	unsigned h;
 	unsigned char *blitdest = NULL, *blitsrc = NULL, *srctiles = NULL, *srclinear = NULL;
 	unsigned xofs = 0, yofs = 0;
 	unsigned xlimit = 0, ylimit = 0;
@@ -128,22 +233,25 @@ void blit_metatile(unsigned char *dest, uint16_t **src, struct animetatile *meta
 	}
 	
 	//~ printf("meta: x=%d y=%d w=%u(%u) h=%u(%u) idx %u\n", meta->x, meta->y, meta->w, metawtiles, meta->h, metahtiles, meta->index);
-	//~ printf("%u %u %u %u\n", meta->x+meta->w < 0, meta->x >= NDS_WIDTH, meta->y+meta->h < 0, meta->y >= NDS_HEIGHT);
+	//~ printf("%u %u %u %u\n", meta->x+meta->w < 0, meta->x >= systemdimensions[state->mode].width, meta->y+meta->h < 0, meta->y >= systemdimensions[state->mode].height);
 	/* check if there is anything to be drawn within bounds */
 	if(meta->x+meta->w < 0) return;
-	if(meta->x >= NDS_WIDTH) return;
+	if(meta->x >= systemdimensions[state->mode].width) return;
 	if(meta->y+meta->h < 0) return;
-	if(meta->y >= NDS_HEIGHT) return;
+	if(meta->y >= systemdimensions[state->mode].height) return;
 	
 	/* adjust x and y offset to be within screen bounds, skip everything outside */
 	if(meta->x < 0) xofs = meta->x * -1;
-	if(meta->x+meta->w >= NDS_WIDTH) xlimit = meta->x+meta->w - NDS_WIDTH;
+	if(meta->x+meta->w >= systemdimensions[state->mode].width) xlimit = meta->x+meta->w - systemdimensions[state->mode].width;
 	
 	if(meta->y < 0) yofs = meta->y * -1;
-	if(meta->y+meta->h >= NDS_HEIGHT) ylimit = meta->y+meta->h - NDS_HEIGHT;
+	if(meta->y+meta->h >= systemdimensions[state->mode].height) ylimit = meta->y+meta->h - systemdimensions[state->mode].height;
 	
 	/* decompress and convert to linear to allow easy off-grid printing */
-	srctiles = deRLE(src[meta->index], metawtiles*metahtiles*tilesize);
+	if(state->isrle) srctiles = deRLE(state->tiledata[meta->index], metawtiles*metahtiles*tilesize);
+	// TEST THIS
+	else srctiles = state->rawtiledata+(tilesize*meta->index);
+	
 	srclinear = generateIndexedImageFromTiles(srctiles, metawtiles, metahtiles, bpp, NULL);
 	
 	if(meta->basepalette) {
@@ -154,23 +262,44 @@ void blit_metatile(unsigned char *dest, uint16_t **src, struct animetatile *meta
 		for(unsigned i = 0; i < meta->w*meta->h; i++) if(srclinear[i]) srclinear[i] += 0x10*meta->twinpalette;
 	}
 	
-	blitdest = dest + meta->x+xofs + imagewidth*(meta->y+yofs);
+	blitdest = dest + meta->x+xofs + systemdimensions[state->mode].width*(meta->y+yofs);
 	blitsrc = srclinear + xofs + meta->w*yofs;
 	
-	//~ printf("dest %p src %p meta %p imw %u bpp %u\n", dest, src, meta, imagewidth, bpp);
-	//~ printf("tilesize %u blitdest %p (offset by %u)\n", tilesize, blitdest, meta->x + imagewidth*meta->y);
+	//~ printf("dest %p src %p meta %p imw %u bpp %u\n", dest, src, meta, systemdimensions[state->mode].width, bpp);
+	//~ printf("tilesize %u blitdest %p blitsrc %p (offset by %d)\n", tilesize, blitdest, blitsrc, meta->x + systemdimensions[state->mode].width*meta->y);
 	//~ printf("xofs=%u xlimit=%u yofs=%u ylimit=%u\n", xofs, xlimit, yofs, ylimit);
 	
-	for(h = 0; h < meta->h-yofs-ylimit; h++) {
-		copynonzero(blitdest+h*imagewidth, blitsrc+h*meta->w, meta->w-xofs-xlimit);
+	for(unsigned h = 0; h < meta->h-yofs-ylimit; h++) {
+		copynonzero(blitdest+h*systemdimensions[state->mode].width, blitsrc+h*meta->w, meta->w-xofs-xlimit);
 	}
 	
-	free(srctiles);
+	if(state->isrle) free(srctiles);
 	free(srclinear);
 }
 
 unsigned isvalidtable(struct animationhead *head) {
-	return !(head->null + head->pad);
+	/* what i assumed to be padding is actually the offset into the tile data used in plain mode
+	   so theres not much left to validate :/ */
+	return !(head->null);
+}
+
+unsigned extractframe(unsigned char *dst, uint32_t *src, struct tiledatastate *state) {
+	uint32_t numparts;
+	uint32_t *partheads = NULL;
+	numparts = *src;
+	partheads = (void *)src + sizeof(uint32_t);
+	
+	for(unsigned curpart = 0; curpart < numparts; curpart++) {
+		struct animetatile metatile;
+		part2animeta(&metatile, &partheads[curpart], state->mode);
+		metatile.twinpalette *= (state->numpals/2);
+		
+		/* convert relative positions to absolute ones */
+		metatile.x += systemdimensions[state->mode].centerx;
+		metatile.y += systemdimensions[state->mode].centery;
+		blit_metatile(dst, &metatile, image4bpp, state);
+	}
+	return numparts;
 }
 
 int main(int argc, char **argv) {
@@ -180,29 +309,41 @@ int main(int argc, char **argv) {
 	unsigned gfxsize, animsize;
 	void *gfxbuf = NULL, *animbuf = NULL;
 	
-	unsigned numpals;
-	unsigned char *palbase = NULL, *paldata = NULL;
-	//~ unsigned char **paldata = NULL;
-	uint16_t **tiledata = NULL;
-	void *gfxbase = NULL;
-	uint32_t *partslist = NULL;
+	struct tiledatastate state = {0};
 	
 	unsigned numtotalframes;
 	struct animationhead *animhead = NULL;
 	struct animationframe *animframes = NULL;
-	uint32_t *numparts;
-	struct partdesc *partheads = NULL;
+	unsigned extractedpartcount = 0;
 	
 	unsigned char *finalimage = NULL;
 	unsigned char *finalrgba = NULL;
 	
-	if(argc < 4) {
-		printf("not enough args! use %s graphics animation outputprefix\n", argv[0]);
+	if(argc < 5) {
+		printf("not enough args! use %s mode graphics animation outputprefix\n", argv[0]);
+		printf("allowed modes are: 'g' for GBA and 'n' for NDS\n");
 		return 1;
 	}
 	
-	if(!(gfx = fopen(argv[1], "rb"))) {
-		printf("couldnt open %s for reading\n", argv[1]);
+	switch(*argv[1]) {
+		case 'g': {
+			printf("mode GBA\n");
+			state.mode = MODE_GBA;
+			break;
+		}
+		case 'n': {
+			printf("mode NDS\n");
+			state.mode = MODE_NDS;
+			break;
+		}
+		default: {
+			printf("unknown mode %c\n", *argv[1]);
+			return 1;
+		}
+	}
+	
+	if(!(gfx = fopen(argv[2], "rb"))) {
+		printf("couldnt open %s for reading\n", argv[2]);
 		return 1;
 	}
 	fseek(gfx, 0, SEEK_END);
@@ -211,20 +352,9 @@ int main(int argc, char **argv) {
 	gfxbuf = malloc(gfxsize);
 	fread(gfxbuf, gfxsize, 1, gfx);
 	fclose(gfx);
-	/* NDS versions have MSB set for some reason, GBA ones dont */
-	numpals = *((uint32_t *)gfxbuf) & 0x7FFFFFFF;
-	//~ paldata = malloc(sizeof(unsigned char *) * numpals);
-	//~ for(unsigned i = 0; i < numpals; i++) paldata[i] = gfxbuf+4+i*0x20;
-	paldata = malloc(0x100*2);
-	memset(paldata, 0, 0x100*2);
-	palbase = gfxbuf+4;
-	memcpy(paldata, palbase, numpals * 0x10*2);
-	partslist = gfxbase = gfxbuf+4+numpals*0x20;
-	tiledata = malloc((partslist[0] / 4)*sizeof(uint16_t *));
-	for(unsigned i = 0; i < (partslist[0] / 4); i++) tiledata[i] = gfxbase+partslist[i];
 	
-	if(!(anim = fopen(argv[2], "rb"))) {
-		printf("couldnt open %s for reading\n", argv[2]);
+	if(!(anim = fopen(argv[3], "rb"))) {
+		printf("couldnt open %s for reading\n", argv[3]);
 		return 1;
 	}
 	fseek(anim, 0, SEEK_END);
@@ -234,77 +364,101 @@ int main(int argc, char **argv) {
 	fread(animbuf, animsize, 1, anim);
 	fclose(anim);
 	
-	finalimage = malloc(NDS_IMAGESIZE);
+	setuptiledatastate(&state, gfxbuf, 0);
 	
-	printf("Graphics file has %u palettes and %u metatiles\n", numpals, partslist[0] / 4);
+	finalimage = malloc(systemdimensions[state.mode].size);
 	
 	numtotalframes = 0;
-	/* only try first table for now */
-	unsigned currenttable, endoftable, lastoffset, lastparts;
+	
+	/* curframe needs to be out here to survive loop iterations for extra frame naming */
+	unsigned currenttable, endoftable, lastoffset, lastparts, curframe = 0;
 	for(currenttable = 0, endoftable = 0; endoftable+sizeof(struct animationhead)-1 < animsize; endoftable += lastoffset + lastparts * sizeof(uint32_t) + sizeof(uint32_t), currenttable++) {
 		lastoffset = 0;
 		lastparts = 0;
 		animhead = animbuf+endoftable;
 		if(!isvalidtable(animhead)) {
-			printf("Table %u has extra frames after the last referenced one! First unreferenced data at %08x\n", currenttable-1, endoftable);
+			/* only when last table */
+			if(endoftable+sizeof(struct animationhead) >= animsize) break;
+			
+			printf("Table %2u has extra frames after the last referenced one! First unreferenced data at %08x\n", currenttable-1, endoftable);
+			if(*(uint32_t *)(animbuf+endoftable) < 64) { // very crude heuristic :/
+				// dump this extra data as a frame
+				/* BEGIN COPYPASTA */
+				memset(finalimage, 0, systemdimensions[state.mode].size);
+				
+				extractedpartcount = extractframe(finalimage, animbuf+endoftable, &state);
+				
+				printf("Extraframe loaded %u parts\n", extractedpartcount);
+				// one ugh housekeeping, make sure extra frame counts towards the table of the earlier ones
+				currenttable--;
+				
+				sprintf(finaloutputname, "%s-tb%02u-fr%02u.png", argv[4], currenttable, curframe);
+				finalrgba = linearImageWithPaletteToRGBA(finalimage, state.paldata, systemdimensions[state.mode].width, systemdimensions[state.mode].height, image8bpp, 0);
+				lodepng_encode32_file(finaloutputname, finalrgba, systemdimensions[state.mode].width, systemdimensions[state.mode].height);
+				free(finalrgba);
+				
+				// more ugh housekeeping, advance frame counter for possibly following extra frames, adjust lastoffset and lastparts to skip over this extra frame
+				curframe++;
+				lastoffset = 0;
+				lastparts = extractedpartcount;
+				// skip remaining code, its possible there may be more extra frames after this one
+				continue;
+				
+				/* END COPYPASTA */
+			}
+			/* this used to search for new usable tables. dumping extraframes results in the same
 			while(endoftable+sizeof(struct animationhead)-1 < animsize && !isvalidtable(animhead)) {
 				endoftable += 4;
 				animhead = animbuf+endoftable;
 			}
-			/* only when last table */
-			if(endoftable+sizeof(struct animationhead) >= animsize) break;
+			*/
+		}
+		
+		if(animhead->tiledataoff != state.statesrc) {
+			if(animhead->tiledataoff > gfxsize) {
+				printf("Table %2u has invalid tiledatasource %08x gfxsize is %08x\n", currenttable, animhead->tiledataoff, gfxsize);
+				currenttable--;
+				break;
+			}
+			printf("Table %2u has new tiledatasource, loading (was %08x, new %08x)\n", currenttable, state.statesrc, animhead->tiledataoff);
+			if(!setuptiledatastate(&state, gfxbuf, animhead->tiledataoff)) {
+				currenttable--;
+				break;
+			}
 		}
 		animframes = (void *)animhead+sizeof(struct animationhead);
-		printf("Table %u at %08x has %u animations\n", currenttable, endoftable, animhead->numentries);
+		printf("Table %2u at %08x has %u animations\n", currenttable, endoftable, animhead->numentries);
 		numtotalframes += animhead->numentries;
 		
-		for(unsigned curframe = 0; curframe < animhead->numentries; curframe++) {
+		for(curframe = 0; curframe < animhead->numentries; curframe++) {
 			if(endoftable + animframes[curframe].offset >= animsize) {
-				printf("Table %u entry %u points to EOF!\n", currenttable, curframe);
+				printf("Table %2u entry %u points to EOF!\n", currenttable, curframe);
 				endoftable += animframes[curframe].offset; // make sure to break out of the outer loop
 				break;
 			}
-			memset(finalimage, 0, NDS_IMAGESIZE);
-			printf("Frame %u dataoff %08x:\n", curframe, animframes[curframe].offset);
-			numparts = (void *)animhead + animframes[curframe].offset;
-			partheads = (void *)numparts + sizeof(uint32_t);
-			for(unsigned curpart = 0; curpart < *numparts; curpart++) {
-				struct animetatile metatile;
-				//~ printf("part %2x x=%4d y=%4d shape=%u size=%u twinpal=%u palnum=%u w=%2u h=%2u tilenum=%4u\n", curpart, partheads[curpart].x, partheads[curpart].y, partheads[curpart].shape, partheads[curpart].size, partheads[curpart].twinpal, partheads[curpart].palnum, sizelut[partheads[curpart].shape][partheads[curpart].size].w, sizelut[partheads[curpart].shape][partheads[curpart].size].h, partheads[curpart].tilenum);
-				if(partheads[curpart].pad) printf("Table %u frame %u part %u has nonzero pad %x!\n", currenttable, curframe, curpart, partheads[curpart].pad);
-				if(partheads[curpart].tilenum >= partslist[0] / 4) printf("Table %u frame %u part %u has OOB index %u!\n", currenttable, curframe, curpart, partheads[curpart].tilenum);
-				//~ fflush(stdout);
-				metatile.x = partheads[curpart].x;
-				metatile.y = partheads[curpart].y;
-				metatile.w = sizelut[partheads[curpart].shape][partheads[curpart].size].w;
-				metatile.h = sizelut[partheads[curpart].shape][partheads[curpart].size].h;
-				metatile.index = partheads[curpart].tilenum;
-				metatile.twinpalette = partheads[curpart].twinpal*(numpals/2);
-				metatile.basepalette = partheads[curpart].palnum;
-				//~ metatile.palette = 0;
-				/* convert relative positions to absolute ones */
-				metatile.x += NDS_CENTER_X;
-				metatile.y += NDS_CENTER_Y;
-				blit_metatile(finalimage, tiledata, &metatile, NDS_WIDTH, image4bpp);
-			}
+			memset(finalimage, 0, systemdimensions[state.mode].size);
 			
-			sprintf(finaloutputname, "%s-tb%02u-fr%02u.png", argv[3], currenttable, curframe);
-			finalrgba = linearImageWithPaletteToRGBA(finalimage, paldata, NDS_WIDTH, NDS_HEIGHT, image8bpp, 0);
-			lodepng_encode32_file(finaloutputname, finalrgba, NDS_WIDTH, NDS_HEIGHT);
+			extractedpartcount = extractframe(finalimage, (void *)animhead + animframes[curframe].offset, &state);
+			
+			printf("Frame %2u dataoff %08x(abs %08lx) loaded %u parts\n", curframe, animframes[curframe].offset, (unsigned char *)animhead + animframes[curframe].offset-(unsigned char *)animbuf, extractedpartcount);
+			
+			sprintf(finaloutputname, "%s-tb%02u-fr%02u.png", argv[4], currenttable, curframe);
+			finalrgba = linearImageWithPaletteToRGBA(finalimage, state.paldata, systemdimensions[state.mode].width, systemdimensions[state.mode].height, image8bpp, 0);
+			lodepng_encode32_file(finaloutputname, finalrgba, systemdimensions[state.mode].width, systemdimensions[state.mode].height);
 			free(finalrgba);
 			
 			if(animframes[curframe].offset > lastoffset) {
 				lastoffset = animframes[curframe].offset;
-				lastparts = *numparts;
+				lastparts = extractedpartcount;
 			}
 		}
 		//~ printf("lastoffset %08x lastparts %08x\n", lastoffset, lastparts);
 	}
-	printf("Found a total of %u tables with %u animations\n", currenttable, numtotalframes);
+	printf("Found a total of %2u tables with %u animations\n", currenttable, numtotalframes);
 	
-	free(finalimage);
-	free(tiledata);
-	free(paldata);
-	free(gfxbuf);
-	free(animbuf);
+	if(finalimage) free(finalimage);
+	if(state.tiledata) free(state.tiledata);
+	if(state.paldata) free(state.paldata);
+	if(gfxbuf) free(gfxbuf);
+	if(animbuf) free(animbuf);
 }
